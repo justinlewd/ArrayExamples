17044f66a62bb23fa478af848af1e88181ab5b2a
justinlewd <43970004+justinlewd@users.noreply.github.com>
2018-10-30 12:25:40 -0700

    factorial

---
+++ factorial.java
@@ -0,0 +1,17 @@
+public class factorial
+{
+    public static void main(String[]args)
+    {
+        
+    }
+    
+    public static int factorial(int n)
+    {
+        System.out.println("DEBUG "+ n);
+        
+        if (n<=1)
+         return 1;
+         
+        return factorial(n-1);
+    }
+}
